86eb396eb9473e199687aa2a8ed2eb3e6dcb5f38
strawsyz <syzyuzhi1119@gmail.com>
2020-10-02 20:33:32 +0900

    improve

--- scripts/clean_empty_dir.py
+++ scripts/clean_empty_dir.py
@@ -33,22 +33,17 @@ def is_only_one_dir(path):
     return False, None
 
 
-# def move_dir(source_path,target_path):
-
-
 def main(path):
     for file_name in os.listdir(path):
         tmp_path = os.path.join(path, file_name)
         if os.path.isdir(tmp_path):
             if is_empty_dir(tmp_path):
-                # 删除空文件夹，如果空文件下面也有空文件夹的话，就无法删除
+                # delete empty folder
                 os.removedirs(tmp_path)
-                # 为了减少依赖，所以不用shutil
-                # import shutil
-                # shutil.rmtree(tmp_path)  # 递归删除文件夹
+                # delete dir
+                # shutil.rmtree(tmp_path)
                 deleted_dir.append(tmp_path)
             else:
-                # 并没有递归的功能
                 result, moved_dir = is_only_one_dir(tmp_path)
                 if result:
                     dirname1 = os.path.basename(tmp_path)
@@ -67,15 +62,13 @@ def main(path):
 
 
 if __name__ == '__main__':
-    """自动删除空文件夹"""
+    """delete empty dir. sort folder where only has one folder"""
     parser = argparse.ArgumentParser(description='delete empty dir')
-    # 默认是当前文件夹
     parser.add_argument('-p', type=str, default=os.getcwd())
     args = parser.parse_args()
     deleted_dir = []
     move_dir = []
     path = args.p
-    path = r"G:\ZKR鉴赏\tmp"
     if not os.path.exists(path):
         print('dir is not exists')
     else:
