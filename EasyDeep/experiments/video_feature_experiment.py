ec3c8b4ea8001014dc22df23e06c89f7e299ae93
strawsyz <syzyuzhi1119@gmail.com>
2022-02-05 15:30:17 +0900

    debug

--- EasyDeep/experiments/video_feature_experiment.py
+++ EasyDeep/experiments/video_feature_experiment.py
@@ -187,6 +187,10 @@ class VideoFeatureExperiment(VideoFeatureConfig, DeepExperiment):
 
 if __name__ == '__main__':
     # config = VideoFeatureConfig()
+    import sys
+
+    sys.path.insert(0, "/workspace/straw/EasyDeep/")
+    # export PYTHONPATH="${PYTHONPATH}:/workspace/straw/EasyDeep/"
 
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     experiment = VideoFeatureExperiment()
