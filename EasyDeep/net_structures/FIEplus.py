bcb7d15444751ae35c69b7067a762c6dd54bdc61
strawsyz <syzyuzhi1119@gmail.com>
2022-03-04 16:52:24 +0900

    debug

--- EasyDeep/net_structures/FIEplus.py
+++ EasyDeep/net_structures/FIEplus.py
@@ -8,8 +8,8 @@
 from torch import nn
 
 from net_structures.old_models import FIE2
-from nets.TorchvisionNet import create_model
-
+# from nets.TorchvisionNet import create_model
+import torch
 
 class FIEISlowFastNet(nn.Module):
     def __init__(self, vocab_size=2048, embedding_dim=256, N=1, heads=1, max_seq_len=200, num_class=101):
@@ -24,3 +24,17 @@ class FIEISlowFastNet(nn.Module):
         # output = self.softmax(output)
         # output = output.squeeze(dim=2)
         return output + output_fie
+
+
+def create_model(model_name, num_target_classes=101, pretrained=True):
+    model = torch.hub.load("facebookresearch/pytorchvideo", model=model_name, pretrained=pretrained)
+
+    layers = list(model.blocks.children())
+    _layers = layers[:-1]
+    # feature_extractor = nn.Sequential(*_layers)
+
+    # 2. Classifier:
+    fc = layers[-1]
+    in_features = layers[-1].proj.in_features
+    fc.proj = nn.Linear(in_features=in_features, out_features=num_target_classes, bias=True)
+    return model
--- EasyDeep/nets/TorchvisionNet.py
+++ EasyDeep/nets/TorchvisionNet.py
@@ -9,7 +9,7 @@ from torch import nn
 
 from base.base_net import BaseNet
 from configs.net_config import VideoFeatureNetConfig
-from net_structures.FIEplus import FIEISlowFastNet
+from net_structures.FIEplus import FIEISlowFastNet, create_model
 
 
 class SlowFastNet(VideoFeatureNetConfig, BaseNet):
@@ -23,16 +23,3 @@ class FIESlowFast(VideoFeatureNetConfig, BaseNet):
         super(FIESlowFast, self).__init__()
         self.net_structure = FIEISlowFastNet(vocab_size=self.vocab_size,embedding_dim=self.embedding_dim,N=self.N, heads=self.heads, num_class=101)
 
-
-def create_model(model_name, num_target_classes=101, pretrained=True):
-    model = torch.hub.load("facebookresearch/pytorchvideo", model=model_name, pretrained=pretrained)
-
-    layers = list(model.blocks.children())
-    _layers = layers[:-1]
-    # feature_extractor = nn.Sequential(*_layers)
-
-    # 2. Classifier:
-    fc = layers[-1]
-    in_features = layers[-1].proj.in_features
-    fc.proj = nn.Linear(in_features=in_features, out_features=num_target_classes, bias=True)
-    return model
